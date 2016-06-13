9f22c1aaf07046dde336f68cb0a83f9b5a293e0c
Adam Mather <adammather@gmail.com>
2016-06-13 10:41:04 +0100

    initial commit
    initial prototype files

---
+++ data_v1.js
@@ -0,0 +1,49 @@
+﻿data = [{
+        "supplier": "Azure",
+        "os": "Linux",
+        "vcpu": "2",
+        "memory": "4GB",
+        "storage": "200GB",
+        "cost": "10.96"
+    },
+    {
+        "supplier": "Azure",
+        "os": "Windows",
+        "vcpu": "6",
+        "memory": "7GB",
+        "storage": "300GB",
+        "cost": "22.34"
+
+    }, {
+        "supplier": "Azure",
+        "os": "Windows",
+        "vcpu": "7",
+        "memory": "15GB",
+        "storage": "400GB",
+        "cost": "12.57"
+    },{
+        "supplier": "Azure",
+        "os": "Linux",
+        "vcpu": "1",
+        "memory": "0.75",
+        "storage": "20",
+        "cost": "12.57"
+    },
+    {
+        "supplier": "Azure",
+        "os": "Linux",
+        "vcpu": "1",
+        "memory": "1.75",
+        "storage": "70",
+        "cost": "37.65"
+    },
+    {
+        "supplier": "Azure",
+        "os": "Linux",
+        "vcpu": "2",
+        "memory": "3.5",
+        "storage": "135",
+        "cost": "75.29"
+    }
+
+]
---
+++ hcc.js
@@ -0,0 +1,962 @@
+﻿var hccdata = [
+        {
+            "supplier": "Azure",
+            "os": "Linux",
+            "vcpu": 1,
+            "memory": 0.75,
+            "storage": 20,
+            "cost": 12.57
+        },
+    {
+        "supplier": "Azure",
+        "os": "Linux",
+        "vcpu": 1,
+        "memory": 1.75,
+        "storage": 70,
+        "cost": 37.65
+    },
+    {
+        "supplier": "Azure",
+        "os": "Linux",
+        "vcpu": 2,
+        "memory": 3.5,
+        "storage": 135,
+        "cost": 75.29
+    },
+    {
+        "supplier": "Azure",
+        "os": "Linux",
+        "vcpu": 4,
+        "memory": 7,
+        "storage": 285,
+        "cost": 150.59
+    },
+    {
+        "supplier": "Azure",
+        "os": "Linux",
+        "vcpu": 8,
+        "memory": 14,
+        "storage": 605,
+        "cost": 301.17
+    },
+    {
+        "supplier": "Azure",
+        "os": "Linux",
+        "vcpu": 2,
+        "memory": 14,
+        "storage": 135,
+        "cost": 150.14
+    },
+    {
+        "supplier": "Azure",
+        "os": "Linux",
+        "vcpu": 4,
+        "memory": 28,
+        "storage": 285,
+        "cost": 313.74
+    },
+    {
+        "supplier": "Azure",
+        "os": "Linux",
+        "vcpu": 8,
+        "memory": 56,
+        "storage": 605,
+        "cost": 627.42
+    },
+    {
+        "supplier": "Azure",
+        "os": "Windows",
+        "vcpu": 1,
+        "memory": 0.75,
+        "storage": 20,
+        "cost": 12.57
+    },
+    {
+        "supplier": "Atos",
+        "os": "Windows",
+        "vcpu": 1,
+        "memory": 1.75,
+        "storage": 70,
+        "cost": 56.47
+    },
+    {
+        "supplier": "Azure",
+        "os": "Windows",
+        "vcpu": 2,
+        "memory": 3.5,
+        "storage": 135,
+        "cost": 112.94
+    },
+    {
+        "supplier": "Azure",
+        "os": "Windows",
+        "vcpu": 4,
+        "memory": 7,
+        "storage": 285,
+        "cost": 225.88
+    },
+    {
+        "supplier": "Azure",
+        "os": "Windows",
+        "vcpu": 8,
+        "memory": 14,
+        "storage": 605,
+        "cost": 451.76
+    },
+    {
+        "supplier": "Azure",
+        "os": "Windows",
+        "vcpu": 2,
+        "memory": 14,
+        "storage": 135,
+        "cost": 207.06
+    },
+    {
+        "supplier": "Azure",
+        "os": "Windows",
+        "vcpu": 4,
+        "memory": 28,
+        "storage": 285,
+        "cost": 414.11
+    },
+    {
+        "supplier": "Azure",
+        "os": "Windows",
+        "vcpu": 8,
+        "memory": 56,
+        "storage": 605,
+        "cost": 828.22
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 1,
+        "memory": 1,
+        "storage": 50,
+        "cost": 57.66
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 1,
+        "memory": 1,
+        "storage": 100,
+        "cost": 64.73
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 1,
+        "memory": 1,
+        "storage": 200,
+        "cost": 78.86
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 1,
+        "memory": 1,
+        "storage": 400,
+        "cost": 107.14
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 1,
+        "memory": 2,
+        "storage": 50,
+        "cost": 69.56
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 1,
+        "memory": 2,
+        "storage": 100,
+        "cost": 76.63
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 1,
+        "memory": 2,
+        "storage": 200,
+        "cost": 90.77
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 1,
+        "memory": 2,
+        "storage": 400,
+        "cost": 119.04
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 2,
+        "memory": 2,
+        "storage": 50,
+        "cost": 85.93
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 2,
+        "memory": 2,
+        "storage": 100,
+        "cost": 93
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 2,
+        "memory": 2,
+        "storage": 200,
+        "cost": 107.14
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 2,
+        "memory": 2,
+        "storage": 400,
+        "cost": 135.41
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 2,
+        "memory": 4,
+        "storage": 50,
+        "cost": 109.74
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 2,
+        "memory": 4,
+        "storage": 100,
+        "cost": 116.81
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 2,
+        "memory": 4,
+        "storage": 200,
+        "cost": 130.94
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 2,
+        "memory": 4,
+        "storage": 400,
+        "cost": 159.22
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 2,
+        "memory": 6,
+        "storage": 50,
+        "cost": 133.55
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 2,
+        "memory": 6,
+        "storage": 100,
+        "cost": 140.62
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 2,
+        "memory": 6,
+        "storage": 200,
+        "cost": 154.75
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 2,
+        "memory": 6,
+        "storage": 400,
+        "cost": 183.02
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 4,
+        "memory": 2,
+        "storage": 50,
+        "cost": 118.67
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 4,
+        "memory": 2,
+        "storage": 100,
+        "cost": 125.74
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 4,
+        "memory": 2,
+        "storage": 200,
+        "cost": 139.87
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 4,
+        "memory": 2,
+        "storage": 400,
+        "cost": 168.14
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 4,
+        "memory": 4,
+        "storage": 50,
+        "cost": 142.48
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 4,
+        "memory": 4,
+        "storage": 100,
+        "cost": 149.54
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 4,
+        "memory": 4,
+        "storage": 200,
+        "cost": 163.68
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 4,
+        "memory": 4,
+        "storage": 400,
+        "cost": 191.95
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 4,
+        "memory": 8,
+        "storage": 50,
+        "cost": 190.09
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 4,
+        "memory": 8,
+        "storage": 100,
+        "cost": 197.16
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 4,
+        "memory": 8,
+        "storage": 200,
+        "cost": 211.3
+    },
+    {
+        "supplier": "TAI",
+        "os": "Windows",
+        "vcpu": 4,
+        "memory": 8,
+        "storage": 400,
+        "cost": 239.57
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 1,
+        "memory": 1,
+        "storage": 50,
+        "cost": 50.96
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 1,
+        "memory": 1,
+        "storage": 100,
+        "cost": 58.03
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 1,
+        "memory": 1,
+        "storage": 200,
+        "cost": 72.17
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 1,
+        "memory": 1,
+        "storage": 400,
+        "cost": 100.44
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 1,
+        "memory": 2,
+        "storage": 50,
+        "cost": 62.87
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 1,
+        "memory": 2,
+        "storage": 100,
+        "cost": 69.94
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 1,
+        "memory": 2,
+        "storage": 200,
+        "cost": 84.07
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 1,
+        "memory": 2,
+        "storage": 400,
+        "cost": 112.34
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 2,
+        "memory": 2,
+        "storage": 50,
+        "cost": 79.24
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 2,
+        "memory": 2,
+        "storage": 100,
+        "cost": 86.3
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 2,
+        "memory": 2,
+        "storage": 200,
+        "cost": 100.44
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 2,
+        "memory": 2,
+        "storage": 400,
+        "cost": 128.71
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 2,
+        "memory": 4,
+        "storage": 50,
+        "cost": 103.04
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 2,
+        "memory": 4,
+        "storage": 100,
+        "cost": 110.11
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 2,
+        "memory": 4,
+        "storage": 200,
+        "cost": 124.25
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 2,
+        "memory": 4,
+        "storage": 400,
+        "cost": 152.52
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 2,
+        "memory": 6,
+        "storage": 50,
+        "cost": 126.85
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 2,
+        "memory": 6,
+        "storage": 100,
+        "cost": 133.92
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 2,
+        "memory": 6,
+        "storage": 200,
+        "cost": 148.06
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 2,
+        "memory": 6,
+        "storage": 400,
+        "cost": 176.33
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 4,
+        "memory": 2,
+        "storage": 50,
+        "cost": 111.97
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 4,
+        "memory": 2,
+        "storage": 100,
+        "cost": 119.04
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 4,
+        "memory": 2,
+        "storage": 200,
+        "cost": 133.18
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 4,
+        "memory": 2,
+        "storage": 400,
+        "cost": 161.45
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 4,
+        "memory": 4,
+        "storage": 50,
+        "cost": 135.78
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 4,
+        "memory": 4,
+        "storage": 100,
+        "cost": 142.85
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 4,
+        "memory": 4,
+        "storage": 200,
+        "cost": 156.98
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 4,
+        "memory": 4,
+        "storage": 400,
+        "cost": 185.26
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 4,
+        "memory": 8,
+        "storage": 50,
+        "cost": 183.4
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 4,
+        "memory": 8,
+        "storage": 100,
+        "cost": 190.46
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 4,
+        "memory": 8,
+        "storage": 200,
+        "cost": 204.6
+    },
+    {
+        "supplier": "TAI",
+        "os": "Linux",
+        "vcpu": 4,
+        "memory": 8,
+        "storage": 400,
+        "cost": 232.87
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 1,
+        "memory": 3.75,
+        "storage": 4,
+        "cost": 54.31
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 2,
+        "memory": 7.5,
+        "storage": 32,
+        "cost": 108.62
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 2,
+        "memory": 3.75,
+        "storage": 32,
+        "cost": 89.28
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 2,
+        "memory": 15.25,
+        "storage": 32,
+        "cost": 137.64
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 4,
+        "memory": 15,
+        "storage": 80,
+        "cost": 217.99
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 4,
+        "memory": 7.5,
+        "storage": 80,
+        "cost": 177.82
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 4,
+        "memory": 30.5,
+        "storage": 80,
+        "cost": 276.02
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 4,
+        "memory": 30.5,
+        "storage": 800,
+        "cost": 697.87
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 8,
+        "memory": 30,
+        "storage": 160,
+        "cost": 435.24
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 8,
+        "memory": 15,
+        "storage": 160,
+        "cost": 355.63
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 8,
+        "memory": 15,
+        "storage": 60,
+        "cost": 522.29
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 8,
+        "memory": 61,
+        "storage": 160,
+        "cost": 551.3
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 8,
+        "memory": 61,
+        "storage": 1600,
+        "cost": 1395.74
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 16,
+        "memory": 30,
+        "storage": 320,
+        "cost": 711.26
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 16,
+        "memory": 122,
+        "storage": 320,
+        "cost": 1102.61
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 16,
+        "memory": 122,
+        "storage": 3200,
+        "cost": 2790.74
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 32,
+        "memory": 60,
+        "storage": 640,
+        "cost": 1422.53
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 32,
+        "memory": 60,
+        "storage": 240,
+        "cost": 2089.15
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 32,
+        "memory": 244,
+        "storage": 640,
+        "cost": 2205.22
+    },
+    {
+        "supplier": "AWS",
+        "os": "Linux",
+        "vcpu": 32,
+        "memory": 244,
+        "storage": 64000,
+        "cost": 5581.49
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 1,
+        "memory": 3.75,
+        "storage": 4,
+        "cost": 95.98
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 2,
+        "memory": 7.5,
+        "storage": 32,
+        "cost": 191.95
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 2,
+        "memory": 3.75,
+        "storage": 32,
+        "cost": 139.87
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 2,
+        "memory": 15.25,
+        "storage": 32,
+        "cost": 215.76
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 4,
+        "memory": 15,
+        "storage": 80,
+        "cost": 384.65
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 4,
+        "memory": 7.5,
+        "storage": 80,
+        "cost": 279.74
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 4,
+        "memory": 30.5,
+        "storage": 80,
+        "cost": 432.26
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 4,
+        "memory": 30.5,
+        "storage": 800,
+        "cost": 723.91
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 8,
+        "memory": 30,
+        "storage": 160,
+        "cost": 768.55
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 8,
+        "memory": 15,
+        "storage": 160,
+        "cost": 559.49
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 8,
+        "memory": 15,
+        "storage": 60,
+        "cost": 570.65
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 8,
+        "memory": 61,
+        "storage": 160,
+        "cost": 803.52
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 8,
+        "memory": 61,
+        "storage": 1600,
+        "cost": 1447.82
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 16,
+        "memory": 30,
+        "storage": 320,
+        "cost": 1118.98
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 16,
+        "memory": 122,
+        "storage": 320,
+        "cost": 1446.34
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 16,
+        "memory": 122,
+        "storage": 3200,
+        "cost": 2894.9
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 32,
+        "memory": 60,
+        "storage": 640,
+        "cost": 2237.95
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 32,
+        "memory": 60,
+        "storage": 240,
+        "cost": 2295.98
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 32,
+        "memory": 244,
+        "storage": 640,
+        "cost": 2604
+    },
+    {
+        "supplier": "AWS",
+        "os": "Windows",
+        "vcpu": 32,
+        "memory": 244,
+        "storage": 64000,
+        "cost": 5789.81
+    }
+]
